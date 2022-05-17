d35233c80082c1b1124b4f4007322632f374402b
prostokeks <tov.camz@gmail.com>
2022-05-17 13:07:34 +0300

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,15 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :users
   resources :account_activations, only: [:edit]
   resources :microposts,          only: [:create, :destroy]
-
+  resources :relationships,       only: [:create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
